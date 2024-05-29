e4055c02b7ec60fbd25a70137307932a87067b42
Carlos Luvina <itlac19122004@lcardenas.tecnm.mx>
2024-05-29 10:30:04 -0600

    latest 29 May

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import tracewLogo from '../assets/tracew.svg';
+import React from "react";
+import { Link } from "react-router-dom";
+import tracewLogo from "../assets/tracew.svg";
 import "../styles/NavBar.css";
 
 const Navbar = () => {
@@ -16,7 +16,34 @@ const Navbar = () => {
             alt=""
           />
         </Link>
-        <NavbarCollapse />
+        <div className="collapse navbar-collapse" id="navbarResponsive">
+          <ul className="navbar-nav ml-auto">
+            <li className="nav-item">
+              <Link className="nav-link link-inicio" to="/">
+                | Inicio
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/login">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="40"
+                  height="40"
+                  fill="currentColor"
+                  className="bi bi-person-circle"
+                  viewBox="0 0 20 20"
+                >
+                  <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0" />
+                  <path
+                    fillRule="evenodd"
+                    d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1"
+                  />
+                </svg>
+                Login
+              </Link>
+            </li>
+          </ul>
+        </div>
       </div>
     </nav>
   );
@@ -31,29 +58,10 @@ const NavbarCollapse = () => {
             | Inicio
           </Link>
         </li>
-        <li className="nav-item">
-          <Link className="nav-link" to="/login">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="40"
-              height="40"
-              fill="currentColor"
-              className="bi bi-person-circle"
-              viewBox="0 0 20 20"
-            >
-              <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0" />
-              <path
-                fillRule="evenodd"
-                d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1"
-              />
-            </svg>
-            Login
-          </Link>
-        </li>
+        {/* Se eliminó el ítem de Login */}
       </ul>
     </div>
   );
 };
 
 export default Navbar;
-
